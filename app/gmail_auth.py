07d28046aafd6ea47a5d3d4852cdcef37ffd2f78
Aditya Naranje <75174363+adityanaranje@users.noreply.github.com>
2026-03-12 18:22:28 +0530

    Update gmail_auth.py

--- app/gmail_auth.py
+++ app/gmail_auth.py
@@ -1,31 +1,59 @@
+import os
+import json
+
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-import os
+
 
 SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
+
 
 def get_gmail_services():
 
     creds = None
 
-    # Load existing token
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # Read token from ENV
+    token_json = os.getenv("GMAIL_TOKEN")
 
-    # If token doesn't exist or expired → login
+    if token_json:
+        creds = Credentials.from_authorized_user_info(
+            json.loads(token_json),
+            SCOPES
+        )
+
+    # If running locally and token file exists
+    elif os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file(
+            "token.json",
+            SCOPES
+        )
+
+    # If token missing → create via OAuth flow
     if not creds or not creds.valid:
 
-        flow = InstalledAppFlow.from_client_secrets_file(
-            "credentials.json", SCOPES
-        )
+        creds_json = os.getenv("GMAIL_CREDENTIALS")
+
+        if creds_json:
+            client_config = json.loads(creds_json)
+
+            flow = InstalledAppFlow.from_client_config(
+                client_config,
+                SCOPES
+            )
+
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json",
+                SCOPES
+            )
 
         creds = flow.run_local_server(port=0)
 
-        # Save token
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-
-    service = build("gmail", "v1", credentials=creds)
+    service = build(
+        "gmail",
+        "v1",
+        credentials=creds
+    )
 
     return service
